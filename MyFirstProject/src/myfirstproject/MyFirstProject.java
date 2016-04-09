9327ed62dca559c5f9adedb8dfdbb912cc58eb30
Wassim <Wassim@Kochat>
2016-04-09 19:18:11 +0200

    delete

--- MyFirstProject/src/myfirstproject/MyFirstProject.java
+++ MyFirstProject/src/myfirstproject/MyFirstProject.java
@@ -16,7 +16,7 @@ public class MyFirstProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Wassim");
+      
     }
     
 }
